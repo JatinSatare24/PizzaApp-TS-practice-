ce8f1504f56af0883c1afa0b813bc140ac60c3e9
Jatin Satare <jatinsatare@gmail.com>
2026-03-24 22:21:54 +0530

    add function placeOrder, TS: type narrowing, explicit object type

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ TYPE ALIAS (Object Shape)
 Using type to define structure of a Pizza object
 */
 type Pizza = {
-name: string
-price: number
+    name: string
+    price: number
 }
 
 /**
@@ -27,9 +27,9 @@ NESTED TYPES
 Using one type (Pizza) inside another (Order)
 */
 type Order = {
-id: number
-pizza: Pizza
-status: OrderStatus
+    id: number
+    pizza: Pizza
+    status: OrderStatus
 }
 
 /**
@@ -40,10 +40,10 @@ ARRAY TYPE ANNOTATION
 Explicitly typing an array of Pizza objects
 */
 const menu: Pizza[] = [
-{ name: "Margherita", price: 8 },
-{ name: "Pepperoni", price: 10 },
-{ name: "Hawaiian", price: 10 },
-{ name: "Veggie", price: 9 },
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
 ]
 
 /**
@@ -72,5 +72,46 @@ FUNCTION PARAMETER TYPE
 Enforcing input type using Pizza
 */
 function addNewPizza(pizzaObj: Pizza) {
-menu.push(pizzaObj)
+    menu.push(pizzaObj)
+}
+
+/**
+
+============================
+FUNCTION WITH STRING PARAM
+
+
+TYPE INFERENCE IN CALLBACK
+
+============================
+*/
+function placeOrder(pizzaName: string) {
+    const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+
+    /**
+    
+    TYPE NARROWING
+    Checking for undefined before using value
+    */
+    if (!selectedPizza) {
+        console.error(`${pizzaName} does not exist in the menu`)
+        return
+    }
+
+    cashInRegister += selectedPizza.price
+
+    /**
+    
+    OBJECT WITH EXPLICIT TYPE
+    Ensures structure matches Order type
+    */
+    const newOrder: Order = {
+        id: nextOrderId++,
+        pizza: selectedPizza,
+        status: "ordered"
+    }
+
+    orderQueue.push(newOrder)
+
+    return newOrder
 }
