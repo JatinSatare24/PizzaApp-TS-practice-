8a0251155bca4802d72579021b18ccf1c25848c8
Jatin Satare <jatinsatare@gmail.com>
2026-03-24 22:14:45 +0530

    Using type to define structure of a Pizza object

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+
+============================
+TYPE ALIAS (Object Shape)
+============================
+Using type to define structure of a Pizza object
+*/
+type Pizza = {
+name: string
+price: number
+}
+
